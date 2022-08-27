9cf30b9c327cad5426792cc2affb232f5cc00f1d
MD. ABRAR MAHIB PRITOM <abrarmahibpritom360@gmail.com>
2022-08-27 15:48:36 +0600

    Way Too Long

---
+++ main.py
@@ -0,0 +1,10 @@
+w = int(input())
+for i in range(0, w):
+    a = str(input())
+    l = len(a)
+    if l > 10:
+        print(a[0],end="")
+        print(l-2, end="")
+        print(a[-1])
+    else:
+        print(a)
